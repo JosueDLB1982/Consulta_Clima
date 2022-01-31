6e4d7db3951bf671107fd07cd3ae8c70ec73690a
Josue_Lopez <josuedlopez1982@gmail.com>
2022-01-31 12:44:29 -0300

    Se elimino la marca personalizada, la haremos luego de otro modo. Creado state que manejara las selecciones del usuario, leyendo la seleccion

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,21 @@
-import React from 'react'
+import { useState } from "react"
 
 const Form = () => {
+    const [search, setSearch] = useState({
+        ciudad: '',
+        pais: ''
+    })
+
+    const {ciudad, pais} = search
+
+    // Función que coloca los elementos en el state
+    const handleChange = e => {
+        setSearch({
+            ...search, // Copia del array actual
+            [e.target.name] : e.target.value //key:value, asigna los que escoja el usuario
+        })
+    }
+
     return (
         <form>
             <div className='input-field col s12'>
@@ -8,6 +23,8 @@ const Form = () => {
                     type='text'
                     name='ciudad'
                     id='ciudad'
+                    value={ciudad}
+                    onChange={handleChange}
                 />
                 <label htmlFor='ciudad'>Ciudad: </label>
             </div>
@@ -16,8 +33,16 @@ const Form = () => {
                 <select
                     name='pais'
                     id='pais'
+                    value={pais}
+                    onChange={handleChange}
                 >
                     <option value=''>-- Seleccione un País --</option>
+                    <option value="US">Estados Unidos</option>
+                    <option value="AR">Argentina</option>
+                    <option value="CO">Colombia</option>
+                    <option value="PE">Perú</option>
+                    <option value="CL">Chile</option>
+                    <option value="VE">Venezuela</option>
                     <label htmlFor='pais'>País: </label>
                 </select>
             </div>
